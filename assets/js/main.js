c578cda04e95a087752b260c80ccdc970f401d6a
Alfredo <alfredogregorio.lozano@gmail.com>
2020-11-30 16:46:42 +0100

    effetto comparsa/scomparsa realizzato

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,38 @@
 var leftBarLinks = $('.left_bar div');
 var rightBarLinks = $('.right_bar div');
 var funzioni = $('.funzioni');
-var hideClass = $('hide_class');
+var planCost = $('.plan_cost');
+var information = $('.information');
+var helpSection = $('.help_section');
+var hideClass = $('.hide_class');
 //aggiunta classi agli elementi dell navbar
 leftBarLinks.addClass('pding');
 rightBarLinks.addClass('pding');
-funzioni.addClass('hide_class');
+
+//hiding sections
+funzioni.hide();
+planCost.hide();
+information.hide();
+helpSection.hide();
+
+// function enter/ leave mouse
+/**
+ * movimento dentro/fuori una sezione
+ * @param {str} section 
+ * @param {string} displaySection 
+ */
+function sectionAppear(section, displaySection) {
+    $(section).mouseenter(function () { 
+        displaySection.show();
+    });
+    $(section).mouseleave(function () { 
+        displaySection.hide();
+    });
+}
+//appears sections
+sectionAppear($('#funzioni'), funzioni);
+sectionAppear($('#abbonamenti'), planCost);
+sectionAppear($('#info'), information);
+sectionAppear($('#help'), helpSection);
+
+

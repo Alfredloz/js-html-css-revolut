f564f60223ee41c37ad2c1aaae9555e8ef138fe8
Alfredo <alfredogregorio.lozano@gmail.com>
2020-11-30 15:55:22 +0100

    inizio e set esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+//crezione variabili
+var leftBarLinks = $('.left_bar div');
+var rightBarLinks = $('.right_bar div');
+var funzioni = $('.funzioni');
+var hideClass = $('hide_class');
+//aggiunta classi agli elementi dell navbar
+leftBarLinks.addClass('pding');
+rightBarLinks.addClass('pding');
+funzioni.addClass('hide_class');
